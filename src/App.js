80142c9ea93cf2b8ed92dc8f09bf4821787c07ee
tarbusca <66177633+tarbusca@users.noreply.github.com>
2020-06-03 13:56:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          
+          <br/>
+          Updated by Tariq Saeed 
         </p>
         <a
           className="App-link"
